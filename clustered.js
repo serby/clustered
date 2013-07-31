249402a4d1f18deddcd3a29c34f513fac9f73284
Tom Gallacher <me@tomg.co>
2013-07-31 10:58:27 +0100

    Correcting undefined pid output

--- clustered.js
+++ clustered.js
@@ -21,7 +21,7 @@ module.exports = function (clusterFn, opts) {
     // Report child process death
     cluster.on('exit', function (worker, code, signal) {
 
-      options.logger.error('Worker ' + worker.pid + ' died with code ' + code, worker)
+      options.logger.error('Worker ' + worker.process.pid + ' died with code ' + code, worker)
 
       if (code === null) {
         cluster.fork()
